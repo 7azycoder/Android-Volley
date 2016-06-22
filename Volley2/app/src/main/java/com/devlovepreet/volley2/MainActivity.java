96516fd997b3ef36d3b7c183d86ec369df66ab61
Lovepreet Singh <dev.lovepreetsingh@gmail.com>
2016-06-22 22:46:20 +0530

    Setting up a Request Queue (other method)

---
+++ Volley2/app/src/main/java/com/devlovepreet/volley2/MainActivity.java
@@ -0,0 +1,62 @@
+package com.devlovepreet.volley2;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import com.android.volley.Cache;
+import com.android.volley.Network;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.BasicNetwork;
+import com.android.volley.toolbox.DiskBasedCache;
+import com.android.volley.toolbox.HurlStack;
+import com.android.volley.toolbox.StringRequest;
+
+public class MainActivity extends AppCompatActivity {
+    Button button;
+    TextView textView;
+    String server_url = "http://192.168.0.101/json/greetings.php";
+    RequestQueue requestQueue;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.bn);
+        textView = (TextView) findViewById((R.id.txt));
+        Cache cache = new DiskBasedCache(getCacheDir(),1024*1024); // 1MB cache memory
+        Network network =  new BasicNetwork(new HurlStack());
+        requestQueue =  new RequestQueue(cache,network);
+        requestQueue.start();
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                StringRequest stringRequest = new StringRequest(Request.Method.POST, server_url,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+                                textView.setText(response);
+                                requestQueue.stop();
+                            }
+                        },
+                        new Response.ErrorListener() {
+                            @Override
+                            public void onErrorResponse(VolleyError error) {
+                                textView.setText("Error");
+                                error.printStackTrace();
+                                requestQueue.stop();
+                            }
+                        });
+                requestQueue.add(stringRequest);
+            }
+
+        });
+
+
+    }
+}
