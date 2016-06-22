b83ccabf87918157f7c98c135e64805aff24c854
Lovepreet Singh <dev.lovepreetsingh@gmail.com>
2016-06-22 23:31:59 +0530

    getting image from server using singleton pattern volley

---
+++ ImageVolley/app/src/main/java/com/devlovepreet/imagevolley/MainActivity.java
@@ -0,0 +1,49 @@
+package com.devlovepreet.imagevolley;
+
+import android.graphics.Bitmap;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.ImageRequest;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button button;
+    ImageView imageView;
+    String server_url = "http://192.168.0.101/json/ninja.jpg";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        button = (Button) findViewById(R.id.bn);
+        imageView = (ImageView)findViewById(R.id.img);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final ImageRequest imageRequest = new ImageRequest(server_url,
+                        new Response.Listener<Bitmap>() {
+                            @Override
+                            public void onResponse(Bitmap response) {
+                                imageView.setImageBitmap(response);
+                            }
+                        }, 0, 0, ImageView.ScaleType.CENTER_CROP, null, new Response.ErrorListener() {
+                    // 0,0 for max image width and height. It means image will take original height and width;
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Toast.makeText(MainActivity.this,"Something went wrong", Toast.LENGTH_SHORT).show();
+                        error.printStackTrace();
+                    }
+                }
+                );
+                MySingleton.getInstance(MainActivity.this).addToRequestQue(imageRequest);
+            }
+        });
+    }
+}
